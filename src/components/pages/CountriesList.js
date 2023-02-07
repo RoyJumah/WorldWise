a37853c9619103e2822b356786b88bb889f4e602
RoyJumah <royjumah17@gmail.com>
2023-02-07 11:25:21 +0300

    Add search functionality

--- src/components/pages/CountriesList.js
+++ src/components/pages/CountriesList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCountries } from '../../redux/countries/CountriesSlice';
@@ -9,9 +9,21 @@ function CountriesList() {
   const { countries, loading, error } = useSelector((state) => state.countries);
   const dispatch = useDispatch();
 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
   React.useEffect(() => {
     dispatch(fetchCountries());
   }, [dispatch]);
+
+  React.useEffect(() => {
+    setFilteredCountries(
+      countries.filter(
+        (country) => country.name.toLowerCase().includes(searchTerm.toLowerCase())
+          || country.alpha3Code.toLowerCase().includes(searchTerm.toLowerCase()),
+      ),
+    );
+  }, [searchTerm, countries]);
 
   if (loading) return <LoadingSpinner />;
   if (error) {
@@ -23,25 +35,32 @@ function CountriesList() {
     );
   }
 
-  const randomCountries = [...countries]
-    .sort(() => 0.5 - Math.random())
-    .slice(0, 12);
-
   return (
-    <div className="countries-list-container">
-      {randomCountries.map((country) => (
-        <div className="country-item" key={country.alpha3Code}>
-          <Link to={`/countries/${country.alpha3Code}`}>
-            <img
-              src={country.flag}
-              alt={country.name}
-              className="country-flag"
-            />
-            {country.name}
-          </Link>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          placeholder="Search for a country..."
+        />
+      </div>
+      <div className="countries-list-container">
+        {filteredCountries.map((country) => (
+          <div className="country-item" key={country.alpha3Code}>
+            <Link to={`/countries/${country.alpha3Code}`}>
+              <img
+                src={country.flag}
+                alt={country.name}
+                className="country-flag"
+              />
+              {country.name}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
