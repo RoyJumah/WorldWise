0d84b2b2090206f6295346c8eab6a53ae6e96f51
RoyJumah <royjumah17@gmail.com>
2023-02-07 21:05:41 +0300

    Add icons

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,23 +1,34 @@
 import { NavLink } from 'react-router-dom';
 import './css/style.css';
+import { MdChevronLeft } from 'react-icons/md';
+import { FaMicrophone, FaCog } from 'react-icons/fa';
 
 const MainHeader = () => (
   <div className="display__flex">
+    <NavLink to="/">
+      <MdChevronLeft size={60} />
+      {' '}
+    </NavLink>
+
     <div className="header">
       <h1 className="page__header">Country Stats</h1>
     </div>
     <nav>
       <ul>
-        <li>
+        {/* <li>
           <NavLink exact to="/">
             Countries
           </NavLink>
-        </li>
+        </li> */}
         {/* <li>
           <NavLink to="/countrieslist">Countries List</NavLink>
         </li> */}
       </ul>
     </nav>
+    <div className="icons">
+      <FaMicrophone size={30} />
+      <FaCog size={30} />
+    </div>
   </div>
 );
 
--- src/components/pages/Countries.js
+++ src/components/pages/Countries.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './Countries.css';
 
