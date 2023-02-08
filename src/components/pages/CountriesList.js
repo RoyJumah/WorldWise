0a2bf6cc67f77a0d16db698352bf49bd5efb504e
RoyJumah <royjumah17@gmail.com>
2023-02-08 09:07:21 +0300

    Add return to homepage icon in selected country page

--- src/components/pages/CountriesList.js
+++ src/components/pages/CountriesList.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+
 import { fetchCountries } from '../../redux/countries/CountriesSlice';
 import './CountriesList.css';
 import LoadingSpinner from '../LoadingSpinner';
